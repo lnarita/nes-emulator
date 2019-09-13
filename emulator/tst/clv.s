fdb6b3fa51e298d15902f48311e00fcfdc2d414a
Lucy Narita <13240125+lnarita@users.noreply.github.com>
2019-09-13 11:19:01 -0300

    fix: typo in test files

--- emulator/tst/clv.s
+++ emulator/tst/clv.s
@@ -44,7 +44,7 @@ MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
    .base $10000-(PRG_COUNT*$4000)
 
 Reset:
-   VLC ; clear overflow flag
+   CLV ; clear overflow flag
 
 NMI:
 
