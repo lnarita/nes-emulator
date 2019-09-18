fa7cd077f70f0f68e8c2ca3637249aaee7885d50
João Vitor Araki Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-09-18 10:39:16 -0300

    adding TAX and TXA unit tests

--- emulator/src/emulator/test/test_opcodes/test_move.py
+++ emulator/src/emulator/test/test_opcodes/test_move.py
@@ -6,11 +6,59 @@ from emulator.constants import AddressingMode
 from emulator.opcodes.opcodes import OpCodes
 
 def test_TXA():
-     print("lalala")
      cpu = CPU()
      memory = Memory()
-     print(OpCodes.all[138].exec(cpu, memory))
+     cpu.a = 1
+     cpu.x = 2
+     TXA = OpCodes.all[138]
+     TXA.exec(cpu, memory)
 
+     assert cpu.a == 2
+     assert cpu.zero == False
+     assert cpu.negative == False
 
-def test_TSA():
-     assert sum([1, 2, 2]) == 5
+     cpu.x = 0
+     cpu.a = 1
+     TXA.exec(cpu, memory)
+
+     assert cpu.a == 0
+     assert cpu.zero == True
+     assert cpu.negative == False
+
+     cpu.x = 0b10000001
+     cpu.a = 1
+     TXA.exec(cpu, memory)
+
+     assert cpu.a == 0b10000001
+     assert cpu.zero == False
+     print(cpu.negative)
+     assert cpu.negative == True
+
+def test_TAX():
+     cpu = CPU()
+     memory = Memory()
+     cpu.x = 1
+     cpu.a = 2
+     TAX = OpCodes.all[170]
+     TAX.exec(cpu, memory)
+
+     assert cpu.x == 2
+     assert cpu.zero == False
+     assert cpu.negative == False
+
+     cpu.a = 0
+     cpu.x = 1
+     TAX.exec(cpu, memory)
+
+     assert cpu.x == 0
+     assert cpu.zero == True
+     assert cpu.negative == False
+
+     cpu.a = 0b10000001
+     cpu.x = 1
+     TAX.exec(cpu, memory)
+
+     assert cpu.x == 0b10000001
+     assert cpu.zero == False
+     print(cpu.negative)
+     assert cpu.negative == True
