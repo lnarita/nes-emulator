68343e8b67f6e36a6ed8061050b9a19469a0f6e2
Lucy Narita <13240125+lnarita@users.noreply.github.com>
2019-09-20 19:52:52 -0300

    fix: clean up imports

--- emulator/src/emulator/test/test_opcodes/test_move.py
+++ emulator/src/emulator/test/test_opcodes/test_move.py
@@ -1,8 +1,5 @@
 from emulator.cpu import CPU
 from emulator.memory import Memory
-import emulator.opcodes.move as move
-from emulator.opcodes.move import MoveOpCodes
-from emulator.constants import AddressingMode
 from emulator.opcodes.opcodes import OpCodes
 
 def test_TXA():
