7cd35124c1e23b33c3352cbc38e7736e96d12ee6
Guilherme Tiaki Sato <guilhermetiaki@me.com>
2019-09-24 19:14:04 -0300

    Merge fix

--- emulator/src/emulator/opcodes/arithmetic.py
+++ emulator/src/emulator/opcodes/arithmetic.py
@@ -7,14 +7,14 @@ from opcodes.base import OpCode
 class ORA(OpCode):
     @classmethod
     def create_variations(cls):
-        variations = [(0x01, AddressingMode.INDIRECT_X, 6,),
-                      (0x05, AddressingMode.ZERO_PAGE, 3,),
-                      (0x09, AddressingMode.IMMEDIATE, 2,),
-                      (0x0D, AddressingMode.ABSOLUTE, 4,),
-                      (0x11, AddressingMode.INDIRECT_Y, 5,),
-                      (0x15, AddressingMode.ZERO_PAGE_X, 4,),
-                      (0x19, AddressingMode.ABSOLUTE_Y, 4,),
-                      (0x1D, AddressingMode.ABSOLUTE_X, 4,)]
+        variations = [(0x01, IndirectX, 6,),
+                      (0x05, ZeroPage, 3,),
+                      (0x09, Immediate, 2,),
+                      (0x0D, Absolute, 4,),
+                      (0x11, IndirectY, 5,),
+                      (0x15, ZeroPageX, 4,),
+                      (0x19, AbsoluteY, 4,),
+                      (0x1D, AbsoluteX, 4,)]
         return map(cls.create_dict_entry, variations)
     def exec(cls, cpu, memory):
         opcode = memory.fetch(cpu.pc-1)
@@ -88,14 +88,14 @@ class ORA(OpCode):
 class AND(OpCode):
     @classmethod
     def create_variations(cls):
-        variations = [(0x21, AddressingMode.INDIRECT_X, 6,),
-                      (0x25, AddressingMode.ZERO_PAGE, 3,),
-                      (0x29, AddressingMode.IMMEDIATE, 2,),
-                      (0x2D, AddressingMode.ABSOLUTE, 4,),
-                      (0x31, AddressingMode.INDIRECT_Y, 5,),
-                      (0x35, AddressingMode.ZERO_PAGE_X, 4,),
-                      (0x39, AddressingMode.ABSOLUTE_Y, 4,),
-                      (0x3D, AddressingMode.ABSOLUTE_X, 4,)]
+        variations = [(0x21, IndirectX, 6,),
+                      (0x25, ZeroPage, 3,),
+                      (0x29, Immediate, 2,),
+                      (0x2D, Absolute, 4,),
+                      (0x31, IndirectY, 5,),
+                      (0x35, ZeroPageX, 4,),
+                      (0x39, AbsoluteY, 4,),
+                      (0x3D, AbsoluteX, 4,)]
         return map(cls.create_dict_entry, variations)
     def exec(cls, cpu, memory):
         opcode = memory.fetch(cpu.pc-1)
@@ -169,14 +169,14 @@ class AND(OpCode):
 class EOR(OpCode):
     @classmethod
     def create_variations(cls):
-        variations = [(0x41, AddressingMode.INDIRECT_X, 6,),
-                      (0x45, AddressingMode.ZERO_PAGE, 3,),
-                      (0x49, AddressingMode.IMMEDIATE, 2,),
-                      (0x4D, AddressingMode.ABSOLUTE, 4,),
-                      (0x51, AddressingMode.INDIRECT_Y, 5,),
-                      (0x55, AddressingMode.ZERO_PAGE_X, 4,),
-                      (0x59, AddressingMode.ABSOLUTE_Y, 4,),
-                      (0x5D, AddressingMode.ABSOLUTE_X, 4,)]
+        variations = [(0x41, IndirectX, 6,),
+                      (0x45, ZeroPage, 3,),
+                      (0x49, Immediate, 2,),
+                      (0x4D, Absolute, 4,),
+                      (0x51, IndirectY, 5,),
+                      (0x55, ZeroPageX, 4,),
+                      (0x59, AbsoluteY, 4,),
+                      (0x5D, AbsoluteX, 4,)]
         return map(cls.create_dict_entry, variations)
     def exec(cls, cpu, memory):
         opcode = memory.fetch(cpu.pc-1)
@@ -250,14 +250,14 @@ class EOR(OpCode):
 class ADC(OpCode):
     @classmethod
     def create_variations(cls):
-        variations = [(0x61, AddressingMode.INDIRECT_X, 6,),
-                      (0x65, AddressingMode.ZERO_PAGE, 3,),
-                      (0x69, AddressingMode.IMMEDIATE, 2,),
-                      (0x6D, AddressingMode.ABSOLUTE, 4,),
-                      (0x71, AddressingMode.INDIRECT_Y, 5,),
-                      (0x75, AddressingMode.ZERO_PAGE_X, 4,),
-                      (0x79, AddressingMode.ABSOLUTE_Y, 4,),
-                      (0x7D, AddressingMode.ABSOLUTE_X, 4,)]
+        variations = [(0x61, IndirectX, 6,),
+                      (0x65, ZeroPage, 3,),
+                      (0x69, Immediate, 2,),
+                      (0x6D, Absolute, 4,),
+                      (0x71, IndirectY, 5,),
+                      (0x75, ZeroPageX, 4,),
+                      (0x79, AbsoluteY, 4,),
+                      (0x7D, AbsoluteX, 4,)]
         return map(cls.create_dict_entry, variations)
     def exec(cls, cpu, memory):
         opcode = memory.fetch(cpu.pc-1)
@@ -343,14 +343,14 @@ class ADC(OpCode):
 class SBC(OpCode):
     @classmethod
     def create_variations(cls):
-        variations = [(0xE1, AddressingMode.INDIRECT_X, 6),
-                      (0xE5, AddressingMode.ZERO_PAGE, 3,),
-                      (0xE9, AddressingMode.IMMEDIATE, 2,),
-                      (0xED, AddressingMode.ABSOLUTE, 4,),
-                      (0xF1, AddressingMode.INDIRECT_Y, 5,),
-                      (0xF5, AddressingMode.ZERO_PAGE_X, 4,),
-                      (0xF9, AddressingMode.ABSOLUTE_Y, 4,),
-                      (0xFD, AddressingMode.ABSOLUTE_X, 4,)]
+        variations = [(0xE1, IndirectX, 6),
+                      (0xE5, ZeroPage, 3,),
+                      (0xE9, Immediate, 2,),
+                      (0xED, Absolute, 4,),
+                      (0xF1, IndirectY, 5,),
+                      (0xF5, ZeroPageX, 4,),
+                      (0xF9, AbsoluteY, 4,),
+                      (0xFD, AbsoluteX, 4,)]
         return map(cls.create_dict_entry, variations)
     def exec(cls, cpu, memory):
         opcode = memory.fetch(cpu.pc-1)
@@ -438,14 +438,14 @@ class SBC(OpCode):
 class CMP(OpCode):
     @classmethod
     def create_variations(cls):
-        variations = [(0xC1, AddressingMode.INDIRECT_X, 6,),
-                      (0xC5, AddressingMode.ZERO_PAGE, 3,),
-                      (0xC9, AddressingMode.IMMEDIATE, 2,),
-                      (0xCD, AddressingMode.ABSOLUTE, 4,),
-                      (0xD1, AddressingMode.INDIRECT_Y, 5,),
-                      (0xD5, AddressingMode.ZERO_PAGE_X, 4,),
-                      (0xD9, AddressingMode.ABSOLUTE_Y, 4,),
-                      (0xDD, AddressingMode.ABSOLUTE_X, 4,)]
+        variations = [(0xC1, IndirectX, 6,),
+                      (0xC5, ZeroPage, 3,),
+                      (0xC9, Immediate, 2,),
+                      (0xCD, Absolute, 4,),
+                      (0xD1, IndirectY, 5,),
+                      (0xD5, ZeroPageX, 4,),
+                      (0xD9, AbsoluteY, 4,),
+                      (0xDD, AbsoluteX, 4,)]
         return map(cls.create_dict_entry, variations)
     def exec(cls, cpu, memory):
         opcode = memory.fetch(cpu.pc-1)
@@ -532,9 +532,9 @@ class CMP(OpCode):
 class CPX(OpCode):
     @classmethod
     def create_variations(cls):
-        variations = [(0xE0, AddressingMode.IMMEDIATE, 2,),
-                      (0xE4, AddressingMode.ZERO_PAGE, 3,),
-                      (0xEC, AddressingMode.ABSOLUTE, 4,)]
+        variations = [(0xE0, Immediate, 2,),
+                      (0xE4, ZeroPage, 3,),
+                      (0xEC, Absolute, 4,)]
         return map(cls.create_dict_entry, variations)
     def exec(cls, cpu, memory):
         elif opcode == 0xE0:
@@ -567,11 +567,10 @@ class CPX(OpCode):
 class CPY(OpCode):
     @classmethod
     def create_variations(cls):
-        variations = [(0xC0, AddressingMode.IMMEDIATE, 2,),
-                      (0xC4, AddressingMode.ZERO_PAGE, 3,),
-                      (0xCC, AddressingMode.ABSOLUTE, 4,)]
+        variations = [(0xC0, Immediate, 2,),
+                      (0xC4, ZeroPage, 3,),
+                      (0xCC, Absolute, 4,)]
         return map(cls.create_dict_entry, variations)
-
     def exec(cls, cpu, memory):
         elif opcode == 0xC0:
             minuend = cpu.y
