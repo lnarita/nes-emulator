bab6c49781195eb157d15b79dca20158620bca6d
Guilherme Tiaki Sato <guilhermetiaki@me.com>
2019-09-26 06:32:21 -0300

    SBC tests

---
+++ tst/sbc_04.s
@@ -0,0 +1,75 @@
+; Author: tokumaru
+; http://forums.nesdev.com/viewtopic.php?%20p=58138#58138
+;----------------------------------------------------------------
+; constants
+;----------------------------------------------------------------
+PRG_COUNT = 1 ;1 = 16KB, 2 = 32KB
+MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
+
+;----------------------------------------------------------------
+; variables
+;----------------------------------------------------------------
+
+   .enum $0000
+
+   ;NOTE: declare variables using the DSB and DSW directives, like this:
+
+   MyVariable0 .dsb 1
+   MyVariable1 .dsb 1
+   
+
+   .ende
+
+   ;NOTE: you can also split the variable declarations into individual pages, like this:
+
+   .enum $0100
+   MyVariable2 .dsb 1
+   MyVariable3 .dsb 1
+   .ende
+
+   ;.enum $0200
+   ;.ende
+
+;----------------------------------------------------------------
+; iNES header
+;----------------------------------------------------------------
+
+   .db "NES", $1a ;identification of the iNES header
+   .db PRG_COUNT ;number of 16KB PRG-ROM pages
+   .db $01 ;number of 8KB CHR-ROM pages
+   .db $00|MIRRORING ;mapper 0 and mirroring
+   .dsb 9, $00 ;clear the remaining bytes
+
+;----------------------------------------------------------------
+; program bank(s)
+;----------------------------------------------------------------
+
+   .base $10000-(PRG_COUNT*$4000)
+
+Reset:
+   LDA #$01
+   STA MyVariable0
+
+   ; Absolute
+   LDA #$0F
+   SEC
+   SBC MyVariable0
+
+
+NMI:
+
+   ;NOTE: NMI code goes here
+
+IRQ:
+
+   ;NOTE: IRQ code goes here
+
+;----------------------------------------------------------------
+; interrupt vectors
+;----------------------------------------------------------------
+
+   .org $fffa
+
+   .dw NMI
+   .dw Reset
+   .dw IRQ
