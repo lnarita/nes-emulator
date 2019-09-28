91ade60f24d1b69ea6e520e762ddde44e601226f
João Vitor Araki Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-09-28 15:35:11 -0300

    LAX opcodes

--- emulator/src/emulator/opcodes/unofficial.py
+++ emulator/src/emulator/opcodes/unofficial.py
@@ -6,6 +6,34 @@ from emulator.cpu import StatusRegisterFlags
 from emulator.opcodes.base import OpCode
 
 
+def unofficial_opcode_str(self, name=None):
+    if name == None:
+        name = type(self).__name__
+
+    def __str_addr():
+        if self.addressing_mode is not None:
+            if self.addressing_mode.low is not None and self.addressing_mode.high is not None:
+                return "{:02X} {:02X}".format(self.addressing_mode.low, (self.addressing_mode.high >> 8))
+            elif self.addressing_mode.low is not None:
+                return "{:02X}".format(self.addressing_mode.low)
+        return ""
+
+    def __str_addr_2():
+        if self.addressing_mode is not None:
+            if self.addressing_mode.addr is not None and self.addressing_mode.data is not None:
+                return "*{} {} {}".format(name, self.addressing_mode.addr, self.addressing_mode.data)
+            elif self.addressing_mode.addr is not None:
+                return "*{} {}".format(name, self.addressing_mode.addr)
+        return "*{}".format(name)
+    with_space = "{:02X} {:<6}{:<30} ".format(self.id, __str_addr(), __str_addr_2())
+    without_space = "{:02X} {:<6}{:<30}".format(self.id, __str_addr(), __str_addr_2())
+
+    if len(with_space) > 40:
+        return without_space
+    else:
+        return with_space
+
+
 class IGN(OpCode):
     @classmethod
     def create_variations(cls):
@@ -40,23 +68,8 @@ class IGN(OpCode):
         _cycle()
 
     def __str__(self):
-        def __str_addr():
-            if self.addressing_mode is not None:
-                if self.addressing_mode.low is not None and self.addressing_mode.high is not None:
-                    return "{:02X} {:02X}".format(self.addressing_mode.low, (self.addressing_mode.high >> 8))
-                elif self.addressing_mode.low is not None:
-                    return "{:02X}".format(self.addressing_mode.low)
-            return ""
+        return unofficial_opcode_str(self, "NOP")
 
-        def __str_addr_2():
-            if self.addressing_mode is not None:
-                if self.addressing_mode.addr is not None and self.addressing_mode.data is not None:
-                    return "{} {} {}".format("*NOP", self.addressing_mode.addr, self.addressing_mode.data)
-                elif self.addressing_mode.addr is not None:
-                    return "{} {}".format("*NOP", self.addressing_mode.addr)
-            return "{}".format(type(self).__name__)
-
-        return "{:02X} {:<6}{:<30} ".format(self.id, __str_addr(), __str_addr_2())
 
 class SKB(OpCode):
     @classmethod
@@ -69,26 +82,10 @@ class SKB(OpCode):
         return map(cls.create_dict_entry, variations)
 
     def exec(self, cpu, memory):
-        address = self.addressing_mode.fetch_address(cpu, memory)
+        _address = self.addressing_mode.fetch_address(cpu, memory)
 
     def __str__(self):
-        def __str_addr():
-            if self.addressing_mode is not None:
-                if self.addressing_mode.low is not None and self.addressing_mode.high is not None:
-                    return "{:02X} {:02X}".format(self.addressing_mode.low, (self.addressing_mode.high >> 8))
-                elif self.addressing_mode.low is not None:
-                    return "{:02X}".format(self.addressing_mode.low)
-            return ""
-
-        def __str_addr_2():
-            if self.addressing_mode is not None:
-                if self.addressing_mode.addr is not None and self.addressing_mode.data is not None:
-                    return "{} {} {}".format("*NOP", self.addressing_mode.addr, self.addressing_mode.data)
-                elif self.addressing_mode.addr is not None:
-                    return "{} {}".format("*NOP", self.addressing_mode.addr)
-            return "{}".format(type(self).__name__)
-
-        return "{:02X} {:<6}{:<30} ".format(self.id, __str_addr(), __str_addr_2())
+        return unofficial_opcode_str(self, "NOP")
 
 
 class NOP(OpCode):
@@ -109,20 +106,54 @@ class NOP(OpCode):
         cpu.exec_in_cycle(_stall)
 
     def __str__(self):
-        def __str_addr():
-            return ""
+        return unofficial_opcode_str(self)
 
-        def __str_addr_2():
-            return "{}".format("*NOP")
 
-        return "{:02X} {:<6}{:<30} ".format(self.id, __str_addr(), __str_addr_2())
+class LAX(OpCode):
+    @classmethod
+    def create_variations(cls):
+        variations = [
+            (0xA3, IndirectX, 6),
+            (0xA7, ZeroPage, 3),
+            (0xAF, Absolute, 4),
+            (0xB3, IndirectY, 5),
+            (0xB7, ZeroPageY, 4),
+            (0xBF, AbsoluteY, 4)]
+        return map(cls.create_dict_entry, variations)
+
+    def exec(self, cpu, memory):
+        def _lda():
+            if self.addressing_mode:
+                address = self.addressing_mode.fetch_address(cpu, memory)
+                value = self.addressing_mode.read_from(cpu, memory, address)
+                if self.addressing_mode != Immediate:
+                    self.addressing_mode.data = "= %02X" % memory.fetch(
+                        address)
+                    cpu.addr = address
+                    cpu.data = value
+
+                cpu.a = value
+                cpu.zero = cpu.a == 0
+                cpu.negative = (cpu.a & 0b10000000) > 0
+
+        def _tax():
+            cpu.x = cpu.a
+            cpu.zero = cpu.x == 0
+            cpu.negative = (cpu.x & 0b10000000) > 0
+
+        _lda()
+        _tax()
+
+    def __str__(self):
+        return unofficial_opcode_str(self)
 
 
 class UnofficialOpcodes:
     opcodes = [
         IGN,
-        NOP, 
-        SKB
+        NOP,
+        SKB,
+        LAX
     ]
 
     @staticmethod
