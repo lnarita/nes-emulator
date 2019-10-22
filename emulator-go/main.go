5958d9c9b6d0dbf8c0dce6d5c815319fbfce445f
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-10-21 22:00:39 -0300

    initial stuff

---
+++ emulator-go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := os.Args
+
+	fmt.Println(args)
+	emulate(args[1])
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func emulate(filePath string) {
+	data, err := ioutil.ReadFile(filePath)
+	check(err)
+
+	fmt.Println(data)
+
+}
