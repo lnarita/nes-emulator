29ca3f5c40d24a44d604e7ca75d528d0f45ac943
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-10-22 21:12:17 -0300

    go constants declaration

--- emulator-go/common/constants.go
+++ emulator-go/common/constants.go
@@ -1,19 +1,21 @@
 package common
 
-// CPUFrequency frequency of the nes cpu
-const CPUFrequency = 1.79e6
+const (
+	// CPUFrequency frequency of the nes cpu
+	CPUFrequency = 1.79e6
 
-// CyclePeriod period of the nes cpu
-const CyclePeriod = 1. / CPUFrequency
+	// CyclePeriod period of the nes cpu
+	CyclePeriod = 1. / CPUFrequency
 
-// KB bits in a kb
-const KB = 1024
+	// KB bits in a kb
+	KB = 1024
 
-// HighBitsMask binary mask for the high 8 bits
-const HighBitsMask = 0b1111111100000000
+	// HighBitsMask binary mask for the high 8 bits
+	HighBitsMask = 0b1111111100000000
 
-// LowBitsMask binary mask for the low 8 bits
-const LowBitsMask = 0b0000000011111111
+	// LowBitsMask binary mask for the low 8 bits
+	LowBitsMask = 0b0000000011111111
 
-// NegativeBit binary mask for the negative bit
-const NegativeBit = 0b0000000010000000
+	// NegativeBit binary mask for the negative bit
+	NegativeBit = 0b0000000010000000
+)
