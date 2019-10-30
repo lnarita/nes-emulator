45fa1a332cf117f4f632cf2ef012fbfe3ed15925
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-10-29 23:13:53 -0300

    draw triangle mock

--- emulator-go/ui/ui.go
+++ emulator-go/ui/ui.go
@@ -3,6 +3,7 @@ package ui
 import (
 	"fmt"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
@@ -10,11 +11,34 @@ import (
 )
 
 const (
-	width  = 256
-	height = 240
-	scale  = 3
-	title  = "GOTEN NES EMULATOR"
-	fps    = 60
+	width              = 256
+	height             = 240
+	scale              = 3
+	title              = "GOTEN NES EMULATOR"
+	fps                = 60
+	vertexShaderSource = `
+    #version 410
+    in vec3 vp;
+    void main() {
+        gl_Position = vec4(vp, 1.0);
+    }
+` + "\x00"
+
+	fragmentShaderSource = `
+    #version 410
+    out vec4 frag_colour;
+    void main() {
+        frag_colour = vec4(1, 1, 1, 1);
+    }
+` + "\x00"
+)
+
+var (
+	triangle = []float32{
+		0, 0.5, 0, // top
+		-0.5, -0.5, 0, // left
+		0.5, -0.5, 0, // right
+	}
 )
 
 func InitUI() {
@@ -45,20 +69,76 @@ func InitUI() {
 	}
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
+	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
 
 	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+
 	gl.LinkProgram(prog)
+	vao := makeVao(triangle)
+
 	for !window.ShouldClose() {
 		t := time.Now()
-		draw(window, prog)
+		draw(vao, window, prog)
 		time.Sleep(time.Second/time.Duration(fps) - time.Since(t))
 
 	}
 }
 
-func draw(window *glfw.Window, program uint32) {
+// makeVao initializes and returns a vertex array from the points provided.
+func makeVao(points []float32) uint32 {
+	var vbo uint32
+	gl.GenBuffers(1, &vbo)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vao
+}
+
+func compileShader(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+func draw(vao uint32, window *glfw.Window, program uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 	gl.UseProgram(program)
+
+	gl.BindVertexArray(vao)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/3))
 
 	glfw.PollEvents()
 	window.SwapBuffers()
