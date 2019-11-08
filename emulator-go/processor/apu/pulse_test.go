36f334e5d7bed3ea6b3deaf259b6738809c49e74
João Vitor Araki Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-11-08 14:32:22 -0300

    some tests

--- emulator-go/processor/apu/pulse_test.go
+++ emulator-go/processor/apu/pulse_test.go
@@ -33,3 +33,24 @@ func TestReadByte1(t *testing.T) {
 		t.Errorf("Expected result of 0b10101010, got %d", byte1)
 	}
 }
+
+func TestWriteByte1(t *testing.T) {
+	p := pulseRegister{}
+	p.writeByte1(0b11111111)
+
+	if p.duty != 0b11 {
+		t.Errorf("Expected duty of 0b11, got %d", p.duty)
+	}
+
+	if !p.envelopeLoop {
+		t.Errorf("Expected envelopeLoop true, got %t", p.envelopeLoop)
+	}
+
+	if !p.constantVolume {
+		t.Errorf("Expected constantVolume true, got %t", p.constantVolume)
+	}
+
+	if p.volume != 0b1111 {
+		t.Errorf("Expected volume of 0b1111, got %d", p.volume)
+	}
+}
